32548385f036cc47600d2bb16b1b82c3ba0eb323
bdurushia <b.durushia@gmail.com>
2024-03-25 11:56:56 -0500

    Completed FizzBuzz whiteboard challenge and put it in it's own method so you can pass your own number into the method call in the Main method.

---
+++ FizzBuzzWhiteboard/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FizzBuzzWhiteboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzzer(200);
+        }
+
+        public static void FizzBuzzer(int num)
+        {
+            for (int i = 1; i <= num; i++)
+            {
+                Console.WriteLine(i % 5 == 0 && i % 3 == 0 ? "FizzBuzz" : i % 5 == 0 ? "Buzz" : i % 3 == 0 ? "Fizz" : $"{i}");
+            }
+        } 
+    }
+}
